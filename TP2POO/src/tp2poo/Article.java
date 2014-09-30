2fbf9486ea8e9c16063fd870d79959eaa906091f
bonnard <jeremylesacri@hotmail.fr>
2014-09-30 15:09:33 +0200

    Exercie 1 Fini

---
+++ TP2POO/src/tp2poo/Article.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2poo;
+/**
+ *
+ * @author Plouf
+ */
+public class Article {
+    protected int reference;
+    protected double prix;
+    protected String designation;
+    //Constructeur par défaut
+    public Article(){
+        super();
+    }
+    /* Constructeur ac paramettre
+        reference
+    */
+    public Article(int reference){
+        this();
+        this.reference = reference;
+    }
+    /*Constructeur ac paramettre:
+        reference
+        designation
+    */
+    public Article (int reference, String designation){
+        this();
+        this.reference = reference;
+        this.designation = designation;
+    }
+    //constructeur complet
+    public Article(int reference, String designation, double prix){
+        this.reference = reference;
+        this.designation = designation;
+        this.prix = prix;        
+    }
+    //getter et setter
+    public int getReference(){
+        return this.reference;        
+    }
+    public double getPrix(){
+        return this.prix;        
+    }
+    public String getDesignation(){
+        return this.designation;
+    }
+    public void setReference(int reference){
+        this.reference = reference;
+    }
+    public void setPrix(double prix){
+        this.prix = prix;
+    }
+    public void setDesignation(String designation){
+        this.designation = designation;
+    }
+    //Fonction to String
+    public String toString(){
+        return getReference()+" "+getDesignation()+" "+getPrix();              
+    }
+    
+    
+}
--- TP2POO/src/tp2poo/TP2POO.java
+++ TP2POO/src/tp2poo/TP2POO.java
@@ -4,10 +4,10 @@
  * and open the template in the editor.
  */
 package tp2poo;
-
+import java.time.Duration;
 /**
  *
- * @author Post
+ * @author Plouf
  */
 public class TP2POO {
 
@@ -16,6 +16,26 @@ public class TP2POO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    //Exercice 1
+    Article a1,a2;
+    a1=new Article();
+        a1.setReference(100);
+        a1.setDesignation("La Programmation en java");
+        a1.setPrix(20.5);
+        
+       a2=new Article(100,"Les méthodes d'agile",25.5);
+       
+        test(a1);
+        System.out.println(a1.toString());
+        
+        test(a2);
+        System.out.println(a2.toString());
+        
     }
-    
+    public static void test(Article a){
+        System.out.println("reference: "+a.getReference());
+        System.out.println("designation :"+a.getDesignation());
+        System.out.println("prix :"+a.getPrix()+" €");
+    }
+     // Exerice 1 fin 
 }
